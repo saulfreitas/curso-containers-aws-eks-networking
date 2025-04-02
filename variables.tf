2cf4d98f316ee667e87e767d2dd38d7b14b3bfa7
Saul Freitas <saulfreitas@gmail.com>
2025-04-02 19:40:18 -0300

    chore: update variables

--- variables.tf
+++ variables.tf
@@ -24,5 +24,23 @@ variable "public_subnets" {
     cidr              = string
     availability_zone = string
   }))
+}
 
+variable "private_subnets" {
+  description = "Lista de Private Subnets da VPC"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
+
+variable "database_subnets" {
+  description = "Lista de Database Subnets da VPC"
+  default     = []
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
 }
